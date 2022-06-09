1a9a36696961a84711084d83c2464165a3a073d3
daiki328 <dk08sel11@i.softbank.jp>
2022-06-09 16:24:53 +0900

    add: something

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/k0kubun/pp"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -17,9 +19,36 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<html><body>hello</body></html>\n")
 }
 
+func handlerCookie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Set-Cookie", "VISIT=TRUE")
+	if _, ok := r.Header["Cookie"]; ok {
+		fmt.Fprintf(w, "<html><body>more than 2</body></html>\n")
+	} else {
+		fmt.Fprintf(w, "<html><body>first</body></html>\n")
+	}
+}
+
+func handlerDigest(w http.ResponseWriter, r *http.Request) {
+	pp.Printf("URL: %s\n", r.URL.String())
+	pp.Printf("Query: %v\n", r.URL.Query())
+	pp.Printf("Proto: %s\n", r.Proto)
+	pp.Printf("Method: %s\n", r.Method)
+	pp.Printf("Header: %v\n", r.Header)
+	defer r.Body.Close()
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Printf("--body--\n%s\n", string(body))
+	if _, ok := r.Header["Authorization"]; !ok {
+		w.Header().Add("WWW-Authenticate", `Digest realm="Secret Zone", qop="auth", nonce="TgLc25", algorithm="MD5"`)
+	} else {
+		fmt.Fprintf(w, "<html><body>hello</body></html>\n")
+	}
+}
+
 func main() {
 	var httpServer http.Server
 	http.HandleFunc("/", handler)
+	//http.HandleFunc("/", handlerCookie)
+	http.HandleFunc("/digest", handlerDigest)
 	log.Println("start http listening :18888")
 	httpServer.Addr = ":18888"
 	log.Println(httpServer.ListenAndServe())
